64642d4a12af7fe4765dccce0e6f93198359b357
Nikhil Jha <hi@nikhiljha.com>
2022-01-02 18:15:51 -0800

    :bug: implement comparable for server try priority

--- plugin/velocity/src/main/kotlin/dev/njha/mycelium/plugin/velocity/models/Server.kt
+++ plugin/velocity/src/main/kotlin/dev/njha/mycelium/plugin/velocity/models/Server.kt
@@ -4,4 +4,19 @@ import kotlinx.serialization.*
 import javax.annotation.Nullable
 
 @Serializable
-data class Server(val name: String, val address: String, @Nullable val host: String, @Nullable val priority: Int)
+data class Server(val name: String, val address: String, @Nullable val host: String?, @Nullable val priority: Int?) : Comparable<Server> {
+    override fun compareTo(other: Server): Int {
+        if (priority == null && other.priority == null) {
+            return 0
+        } else if (priority == null) {
+            return -1
+        } else if (other.priority == null) {
+            return 1
+        } else if (priority < other.priority) {
+            return -1
+        } else if (other.priority > priority) {
+            return 1
+        }
+        return 0
+    }
+}
